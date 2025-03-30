f37a95c313b6419eb9e8f32576f6af616feee35b
mohamed-chafik <mchafik2006@gmail.com>
2025-03-30 12:42:53 +0000

    init

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+from flask import render_template  # <- This imports HTML templates
+from app import app  # Import the Flask app
+
+@app.route("/")
+def home():
+    return render_template('index.html', title='Home Page') 
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About Us")
