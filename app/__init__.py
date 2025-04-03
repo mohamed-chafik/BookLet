186d1da7b4a99f08c0e8afe73e59b8a26b033d70
mohamed-chafik <mchafik2006@gmail.com>
2025-04-03 13:17:08 +0000

    test

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 app = Flask(__name__)  # Create the Flask app
 app.config.from_object('config')  # Load settings from config.py
 
+#
 # Import routes at the end to avoid circular imports
 from app import routes
