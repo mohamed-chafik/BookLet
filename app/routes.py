8b42b26fd1d1a2230580545b8074ab28d49f6089
mohamed-chafik <mchafik2006@gmail.com>
2025-04-07 10:46:25 +0100

    adding app

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from app import app  # Import the Flask app
 
 @app.route("/")
 def home():
-    return render_template('index.html', title='Home Page') 
+    return render_template('questions.html', title='Home Page') 
 
 @app.route("/about")
 def about():
